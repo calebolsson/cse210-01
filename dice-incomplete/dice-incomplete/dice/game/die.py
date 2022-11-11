529767d213537a0c19211b4710447912922038ee
calebolsson <caleb.l.olsson@gmail.com>
2022-11-10 19:35:06 -0600

    dice game completion

--- dice-incomplete/dice-incomplete/dice/game/die.py
+++ dice-incomplete/dice-incomplete/dice/game/die.py
@@ -4,17 +4,23 @@ import random
 # TODO: Implement the Die class as follows...
 
 # 1) Add the class declaration. Use the following class comment.
+class Die:
     """A small cube with a different number of spots on each of its six sides.
 
     The responsibility of Die is to keep track of the side facing up and calculate the points for 
     it.
-   
+
     Attributes:
         value (int): The number of spots on the side facing up.
         points (int): The number of points the die is worth.
     """
 
 # 2) Create the class constructor. Use the following method comment.
+    def __init__(self, default=6):
+        self.value = 0
+        self.points = 0
+        self.sides = default
+        self.scoremap = [100, 0, 0, 0, 50, 0]
         """Constructs a new instance of Die with a value and points attribute.
 
         Args:
@@ -22,6 +28,9 @@ import random
         """
 
 # 3) Create the roll(self) method. Use the following method comment.
+    def roll(self):
+        self.value = random.randrange(1, self.sides)
+        self.points = self.scoremap[self.value - 1]
         """Generates a new random value and calculates the points.
         
         Args:
